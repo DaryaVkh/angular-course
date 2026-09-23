9b6e3c45901c30f0827d0a6265fcf6d0396a0f09
RevaVlad <revavlad205@gmail.com>
2026-09-24 00:26:07 +0500

    feat: добавить мигание кнопки

--- lessons/components-and-templates/tasks/projection/src/app/ui/card/card.component.ts
+++ lessons/components-and-templates/tasks/projection/src/app/ui/card/card.component.ts
@@ -1,8 +1,11 @@
 import {
+  AfterViewInit,
   ChangeDetectionStrategy,
   Component,
+  ElementRef,
   input,
   output,
+  viewChild,
 } from '@angular/core';
 import { ListItemComponent } from '../list-item/list-item.component';
 
@@ -16,10 +19,20 @@ import { ListItemComponent } from '../list-item/list-item.component';
     class: 'flex w-fit flex-col gap-3 rounded-md border-2 border-black p-4',
   },
 })
-export class CardComponent<T extends { id: number }> {
+export class CardComponent<T extends { id: number }> implements AfterViewInit {
   readonly list = input<T[] | null>(null);
   readonly getName = input.required<(item: T) => string>();
 
   readonly addOne = output<void>();
   readonly deleteOne = output<number>();
+
+  readonly addButton = viewChild<ElementRef<HTMLButtonElement>>('addButton');
+
+  ngAfterViewInit(): void {
+    this.addButton()?.nativeElement.classList.add('flash');
+    setTimeout(
+      () => this.addButton()?.nativeElement.classList.remove('flash'),
+      600,
+    );
+  }
 }
